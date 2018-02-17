1e75336df7cf3aefc8c6b961d9a8f44282e45a53
hy yang <almond2yang@gmail.com>
2018-02-17 01:31:42 -0800

    json utility with jackson

---
+++ src/main/java/utilitises/JSONConverter.java
@@ -0,0 +1,15 @@
+package utilitises;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JSONConverter {
+
+	private static final ObjectMapper mapper = new ObjectMapper(); 
+	
+	public static String object2JSON(Object o) throws JsonProcessingException {
+		String res= mapper.writeValueAsString(o);
+		return res;
+	}
+	
+}
