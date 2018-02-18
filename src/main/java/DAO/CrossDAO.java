b3819fd27165963d4c761225fab0370e399b2f80
hy yang <almond2yang@gmail.com>
2018-02-17 17:50:29 -0800

    crossDAO for course enrolling and dropping

---
+++ src/main/java/DAO/CrossDAO.java
@@ -0,0 +1,15 @@
+package DAO;
+
+public class CrossDAO {
+	
+	public static void studentEnrollCourse(int sID, String cID) {
+		StudentDAO.getStudentByID(sID).enrollCourse(cID);
+		CourseDAO.getCourseByID(cID).enrollStudent(sID);
+	}
+	
+	public static void studentDropCourse(int sID, String cID) {
+		StudentDAO.getStudentByID(sID).dropCourse(cID);
+		CourseDAO.getCourseByID(cID).dropStudent(sID);
+	}
+
+}
